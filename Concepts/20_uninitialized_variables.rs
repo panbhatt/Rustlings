25e9474c7905b670323b882154074708c18fadc6
“pankaj <panbhatt@gmail.com>
2023-05-31 15:57:33 -0400

    20 -> Uninitialized variables. You can't use them.

---
+++ Concepts/20_uninitialized_variables.rs
@@ -0,0 +1,8 @@
+
+
+fn main() {
+    //let x: usize ; // This is an error, Rust prohibits you from using a variables unless it is being touched. so we have to explicitly initialize it. 
+    let x: i32 = 999 ; 
+
+    println!("{}", x);  
+}
