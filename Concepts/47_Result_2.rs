4d483131e4ec3bd4f42ab1497acb81476d105e3f
“pankaj <panbhatt@gmail.com>
2023-06-09 22:40:09 -0400

    45,46,47 -> Result with Turbofish and ParseIntError

---
+++ Concepts/47_Result_2.rs
@@ -0,0 +1,29 @@
+// TUROBFISH & FIND THE ERROR TYPE
+
+use std::num::ParseIntError;
+
+fn parse_integer(s: &str) -> Result<isize, ParseIntError> {
+    s.parse::<isize>() // THis is called TURBOFISH ::<> because it looks like turbofish.
+}
+
+fn main() {
+    let my_number = parse_integer("123456789");
+    if my_number.is_ok() {
+        println!("{}", my_number.unwrap());
+    } else {
+        println!("No Valid Number passed");
+    }
+
+    println!("========================================="); 
+    let numbers = vec!["5", "Pankaj", "67", "sixtyseven", "eight", "8989"]; 
+
+    for number in numbers {
+        let result = parse_integer(number); 
+        let result_number = match result {
+            Ok(n) => println!("{}", n) ,
+            Err(e) => println!("{}", e), 
+        };
+    } 
+
+
+}
