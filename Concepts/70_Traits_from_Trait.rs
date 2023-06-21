6ad8b38e856ed9e66af99166ff94ab47b970f353
“pankaj <panbhatt@gmail.com>
2023-06-20 23:58:11 -0400

    70 -> Adding another function in the Orgranization that prints all the Employees

---
+++ Concepts/70_Traits_from_Trait.rs
@@ -0,0 +1,53 @@
+// Implementation of FROM Trait .
+
+// we are going to create an organization from a vec of Employees
+
+#[derive(Debug)]
+struct Employee {
+    name: String,
+    age: u8,
+}
+
+impl Employee {
+    fn new(name: &str, age: u8) -> Self {
+        Employee {
+            name: name.to_string(),
+            age,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Organization {
+    employees: Vec<Employee>,
+}
+
+impl From<Vec<Employee>> for Organization {
+    // This is the implementation Trait FROM for ORgranziation
+    fn from(emps: Vec<Employee>) -> Self {
+        Self { employees: emps }
+    }
+}
+
+impl Organization {
+    fn print_employees(&self) {
+        println!("===================PRINTING ALL EMPLOYEES=================="); 
+        for emp in &self.employees {
+            println!("{:?}", emp);
+        }
+    }
+}
+
+fn main() {
+    let emp1 = Employee::new("Pankaj", 34);
+    let emp2 = Employee::new("Joh7", 34);
+    let emp3 = Employee::new("Jane", 28);
+
+    let emp_vec = vec![emp1, emp2, emp3];
+
+    let org = Organization::from(emp_vec);
+
+    println!("Length of ORg EMployees -> {}", org.employees.len());
+
+    org.print_employees(); 
+}
