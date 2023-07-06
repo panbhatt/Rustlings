323f91ed6ce1bd6a5782c714961b6b8103d1cf0d
“pankaj <panbhatt@gmail.com>
2023-07-06 15:51:10 -0400

    100 -> Static lifetime and normal &str

---
+++ Concepts/100_lifetime.rs
@@ -0,0 +1,29 @@
+// 'static
+// 'a 
+
+#[derive(Debug)]
+struct City {
+    name : &'static str,  // If thi sis &str, but it is going to give error that expected names lifetime parameter. , but static will work. 
+    date_founded : u32, 
+}
+
+fn main() {
+    let dehradun = City {
+        name : "Dehradun",
+        date_founded : 1850,
+    };
+
+    let jaipur = City {
+        name : "Jaipur",
+        date_founded : 1850,
+    };
+
+    println!("{:#?}, {:#?}", dehradun, jaipur); // TILL THIS WORKS. 
+
+    let cities = vec!["Miami".to_string()]; 
+
+    let delhi = City {
+        name : &cities[0],    // This wont work as we are expecting name to be static i.e. lived throughout the program, but it is going away. 
+        date_founded : 1660, 
+    };
+}
