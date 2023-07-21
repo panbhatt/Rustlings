c5f5ccbbdb9a8600618b471292708b03d4cbb4a0
“pankaj <panbhatt@gmail.com>
2023-07-21 16:02:18 -0400

    134 -> Channel with Threads and Cloning

---
+++ Concepts/134_Channel_with_threads.rs
@@ -0,0 +1,35 @@
+// Channels with Threads.
+
+use std::sync::mpsc::channel;
+use std::thread::spawn;
+
+fn main() {
+    let (sender, receiver) = channel(); // Arc Mutex can be used here. 
+    let sender2 = sender.clone();
+    let sender3 = sender.clone();
+
+    let mut handle_vec = vec![];
+
+    handle_vec.push(spawn(move || {
+        sender.send(10);
+    }));
+
+    handle_vec.push(spawn(move || {
+        sender2.send(100);
+    }));
+
+    handle_vec.push(spawn(move || {
+        sender3.send(1000);
+    }));
+
+    // Without this, our main thread is not going to be joined by child thread. 
+    for handle in handle_vec {
+        handle.join(); 
+    }
+
+    // Since the threads can be executed in any order, the output is unpredictable
+
+    println!("{:?}", receiver.try_recv());
+    println!("{:?}", receiver.try_recv());
+    println!("{:?}", receiver.try_recv());
+}
