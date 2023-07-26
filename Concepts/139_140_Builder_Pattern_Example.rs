909f76378db74e6aafea16b1a99041a636de66aa
“pankaj <panbhatt@gmail.com>
2023-07-26 14:16:41 -0400

    Changing the name of the 140 portion

--- Concepts/139_140_Builder_Pattern_Example.rs
+++ Concepts/139_140_Builder_Pattern_Example.rs
@@ -1,5 +1,4 @@
-// Builder Pattern. 
-
+// Builder Pattern.
 
 #[derive(Debug)]
 enum WeekDay {
@@ -15,9 +14,9 @@ enum WeekDay {
 #[derive(Debug)]
 struct Employee {
     name: String,
-    age : u8, 
+    age: u8,
     birth_day: WeekDay,
-    can_use : bool, 
+    can_use: bool, // will turn true, only if you call build function. 
 }
 
 impl Employee {
@@ -30,34 +29,33 @@ impl Employee {
             } else {
                 WeekDay::MONDAY
             },
-            age : 0, 
-            can_use : true, 
+            age: 0,
+            can_use: true,
         }
     }
 
-    // BUILDER PATTERN. 
-    fn name(mut self, name : String) -> Self {
+    // BUILDER PATTERN.
+    fn name(mut self, name: String) -> Self {
         self.name = name;
         self
     }
 
-    fn age(mut self, age : u8) -> Self {
+    fn age(mut self, age: u8) -> Self {
         self.age = age;
         self
     }
 
-    // This function is end of the builder pattern that verifies all the input parameters before creating the object. 
+    // This function is end of the builder pattern that verifies all the input parameters before creating the object.
     fn build(mut self) -> Result<Self, String> {
         // This will chk the can_use variable to define
         if self.age > 40 {
-            self.can_use = false; 
+            self.can_use = false;
             Err("Your Age can't be less then 40".into())
         } else {
-            self.can_use = true; 
+            self.can_use = true;
             Ok(self)
         }
     }
-
 }
 
 // IMPL DEFAULT for Employee
@@ -66,8 +64,8 @@ impl Default for Employee {
         Self {
             name: "KUPUTRA GUPTA".into(),
             birth_day: WeekDay::SUNDAY,
-            age : 0, 
-            can_use : true, 
+            age: 0,
+            can_use: true,
         }
     }
 }
@@ -86,10 +84,14 @@ fn main() {
     let john_abhram = Employee::default().name("JOHN ABHRAHAM".into()).age(30);
     println!("JOHN -> {:?}", john_abhram);
 
-     let hrithink_roshan_result = Employee::default().name("JHRITHINK ROSHAN".into()).age(45).build();
-     match hrithink_roshan_result {
-        Ok(hr) => println!("HR -> {:?}", hr), 
-        Err(err) => println!("ERROR ->> {}", err), 
-     }; 
+    // The is with builder method that would give us the error when we do make build ()
+    let hrithink_roshan_result = Employee::default()
+        .name("JHRITHINK ROSHAN".into())
+        .age(45)
+        .build();
+    match hrithink_roshan_result {
+        Ok(hr) => println!("HR -> {:?}", hr),
+        Err(err) => println!("ERROR ->> {}", err),
+    };
     // println!("H ROSHAN -> {:?}", hrithink_roshan);
 }
