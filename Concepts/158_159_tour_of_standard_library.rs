52ce04f79272323bf15a5eee667d5e38eabf6cf8
“pankaj <panbhatt@gmail.com>
2023-08-02 23:53:04 -0400

    159 -> Char and integers with try_from and checked functions.

--- Concepts/158_159_tour_of_standard_library.rs
+++ Concepts/158_159_tour_of_standard_library.rs
@@ -1,6 +1,8 @@
 // Tour of standard library contains.
 // Arrays, bool, char, Vec
 
+use std::convert::TryFrom; 
+
 fn main() {
     let cities = ["Delhi", "MIAMI", "SFO"]; // or use & in front of [];
                                             // you can't iterate over an Error
@@ -19,5 +21,19 @@ fn main() {
     for character in hindi_word.chars() {
         println!("character {} -> {}", character, character.escape_unicode()); 
     }
+
+    println!("Char from ->  {}", char::from(97)); // value has to u8
+    println!("U8 Max : {}", u8::MAX); 
+
+    println!("{:?}", char::try_from(345u32).unwrap_or('?')) ; // 345 treat me as u32. 
+
+    println!("======================== INTEGERS =========================="); 
+    // checked_add /mul/ div/ sub () -> 
+
+    let some_number = 200_u8; 
+    let other_number = 200_u8; 
+
+    println!("ADD  with checked {:?}", some_number.checked_add(other_number)); // Results NONE as it is an ERROR. since A +B -> wont work. 
+
 
 }
