f34d2d860a2cd5142850e2e1a3f05080792a1d83
“pankaj <panbhatt@gmail.com>
2023-08-10 15:51:26 -0400

    181 -> Taking input from the user and read Args

--- Concepts/181_Takeing_Input_from_User.rs
+++ Concepts/181_Takeing_Input_from_User.rs
@@ -1,10 +1,15 @@
+use std::env::args;
 use std::io;
 
-// RUn this program from below command 
-// rustc 181_Takeing_Input_from_User.rs  && ./181_Takeing_Input_from_User 
+// RUn this program from below command
+// rustc 181_Takeing_Input_from_User.rs  && ./181_Takeing_Input_from_User
 
 fn main() {
     let mut input = String::new();
+
+    print_args(); 
+
+    println!("======= Start Entering Data ====="); 
 
     while input.trim() != "quit" {
         input.clear();
@@ -12,5 +17,13 @@ fn main() {
         io::stdin().read_line(&mut input).unwrap();
         println!("You wrote -> {:?}", input.trim());
         //input.trim(); // to wipe of \r \n
+    }
+}
+
+fn print_args() {
+    let cmd_args = args().skip(1); ;  // To remove the name of the program . 
+    println!("======= Command Line Arguments ====="); 
+    for entry in cmd_args {
+        println!("{:?}", entry);
     }
 }
