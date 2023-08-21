968c3ab124dc78d1abc955b104aca1d64feea619
“pankaj <panbhatt@gmail.com>
2023-08-21 13:49:03 -0400

    191 -> Implementing try_get_one() function to return ONE and Err on TWO

--- Concepts_186/src/191_Implementating_Enums_as_traits_2.rs
+++ Concepts_186/src/191_Implementating_Enums_as_traits_2.rs
@@ -1,26 +1,30 @@
-
-// This will be same as that of PREVIOUS one, just that we are going to use ENUM as a replacement of TRAIT. 
+// This will be same as that of PREVIOUS one, just that we are going to use ENUM as a replacement of TRAIT.
 
 enum GiveName {
-    One(One), 
-    Two(Two)
+    One(One),
+    Two(Two),
 }
 
 impl GiveName {
-
     fn give_name(&self) -> &'static str {
         match self {
-            GiveName::One(o) => o.give_name(), 
-            GiveName::Two(t) => t.give_name() , 
+            GiveName::One(o) => o.give_name(),
+            GiveName::Two(t) => t.give_name(),
         }
+    }
 
+    fn try_get_one(&self) -> Result<One, String> {
+        match self {
+            GiveName::One(o) => Ok(o.clone()),
+            GiveName::Two(__) => Err("Some Unknown type is being passed in".to_string()),
+        }
     }
 }
 
-#[derive(Debug)]
-struct One; 
-#[derive(Debug)]
-struct Two; 
+#[derive(Debug,Copy, Clone)]
+struct One;
+#[derive(Debug,Copy, Clone)]
+struct Two;
 
 impl One {
     fn give_name(&self) -> &'static str {
@@ -35,10 +39,12 @@ impl Two {
 }
 
 fn main() {
-    let o = GiveName::One(One{}); 
-    let t = GiveName::Two(Two{}); 
+    let o = GiveName::One(One {});
+    let t = GiveName::Two(Two {});
 
-    println!("ONE RETURNING -> {}", o.give_name())  ; 
-    println!("TWO RETURNING -> {}", t.give_name())  ; 
+    println!("ONE RETURNING -> {}", o.give_name());
+    println!("TWO RETURNING -> {}", t.give_name());
 
+    println!("One -> Try One -> {:?}", o.try_get_one());
+    println!("Two -> Try TWO -> {:?}", t.try_get_one());
 }
