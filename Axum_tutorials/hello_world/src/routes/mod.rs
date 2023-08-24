f2f58ded17222fc213e2b5204f3f37e7fc72f0e7
“pankaj <panbhatt@gmail.com>
2023-08-24 15:39:05 -0400

    AXUM -> Parse String -> JSON and Return a JSON

--- Axum_tutorials/hello_world/src/routes/mod.rs
+++ Axum_tutorials/hello_world/src/routes/mod.rs
@@ -1,12 +1,19 @@
 
 mod hello_world; 
+mod user_controller; 
 
 use axum::{Router, routing::{get,post}} ; 
 
 use hello_world::hello_universe; 
+use user_controller::{post_user_data, post_user_data_json }; 
 
 pub fn create_routes() -> Router<>{
-    let  router = Router::new().route("/", get(hello_world)).route("/hello", get(|| async { "HELLO"})).route("/universe", post(hello_universe));
+    let  router = Router::new()
+    .route("/", get(hello_world))
+    .route("/hello", get(|| async { "HELLO"}))
+    .route("/universe", post(hello_universe))
+    .route("/user", post(post_user_data))
+    .route("/user/create", post(post_user_data_json)); 
     return router;
 
 }
---
+++ Axum_tutorials/hello_world/src/routes/user_controller.rs
@@ -0,0 +1,37 @@
+
+
+use axum::Json; 
+use serde::{Serialize, Deserialize};
+use chrono::{Utc, DateTime} ; 
+
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct User_Create_Request {
+    name : String, 
+    username : String, 
+}
+
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User_Create_Response {
+    username : String, 
+    createdAt : String, 
+}
+
+
+pub async fn post_user_data(body : String) -> String{
+    println!("RECEIVING THE BODY = {}", body); 
+    body
+}
+
+pub async fn post_user_data_json(Json(body) : Json<User_Create_Request>) -> Json<User_Create_Response>{
+    dbg!(body.clone()) ; 
+    let response = User_Create_Response {
+        username : body.username, 
+        createdAt : Utc::now().to_rfc2822() 
+
+    };
+
+    Json(response)
+    
+}
