88037d4dd10a5c71faf7d5d061f95843b7c18e4e
“pankaj <panbhatt@gmail.com>
2023-08-29 15:48:12 -0400

    AXUM -> Changed the previous commit msg to a IM_A_TEAPOT

--- Axum_tutorials/hello_world/src/routes/custom_statuscodes.rs
+++ Axum_tutorials/hello_world/src/routes/custom_statuscodes.rs
@@ -2,7 +2,9 @@ use axum::http::StatusCode;
 
 
 pub async fn set_custom_status_code() -> Result<(), StatusCode> {
-    Err(StatusCode::BAD_REQUEST)
+
+    Err(StatusCode::IM_A_TEAPOT)
+    //    Err(StatusCode::BAD_REQUEST)
     //Ok(()), for 200 Status code. 
 
 }
