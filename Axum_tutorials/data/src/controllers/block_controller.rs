c51c14c2860a81b8aa8e98c1a8b9bb6680825faf
“pankaj <panbhatt@gmail.com>
2023-09-13 14:13:38 -0400

    AXUM -> Adding changes for FILTERING before doing findALL()
    HTTP API: 
    http "localhost:3000/api/blocks?txCount=120" 
    Will only get the blocks where txCount=120

--- Axum_tutorials/data/src/controllers/block_controller.rs
+++ Axum_tutorials/data/src/controllers/block_controller.rs
@@ -1,8 +1,17 @@
 use axum::extract::Path;
+use axum::extract::Query;
+use axum::http::header::GetAll;
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
 use axum::{Extension, Json};
 use log::info;
+use sea_orm::sea_query::Expr;
+use sea_orm::sea_query::IntoCondition;
+use sea_orm::Condition;
+use sea_orm::Order;
+use sea_orm::PaginatorTrait;
+use sea_orm::QueryFilter;
+use sea_orm::QueryOrder;
 use sea_orm::{ActiveModelTrait, DatabaseConnection, EntityTrait, Set};
 //use crate::models::prelude::Blocks;
 //use super::super::models::blocks;
@@ -108,12 +117,55 @@ pub async fn create_block(
         .into_response()
 }
 
+//Filters for below code.
+#[derive(Deserialize)]
+pub struct GetAllBlockQueryParams {
+    size: Option<u8>,
+    page: Option<u8>,
+    txCount: Option<u8>,
+}
 /**
  * This function will return all the blocks.
  */
-pub async fn get_all_blocks(Extension(db): Extension<DatabaseConnection>) -> Response {
+pub async fn get_all_blocks(
+    Extension(db): Extension<DatabaseConnection>,
+    Query(query_params): Query<GetAllBlockQueryParams>,
+) -> Response {
     info!("Starting to get the get_all_blocks Data");
-    let all_blocks = BlockEntity::find().all(&db).await.unwrap();
+
+    let mut tx_count_filter = Condition::all();
+
+    if let Some(txCount) = query_params.txCount {
+        //tx_count_condition =         tx_count_condition.add(Expr::col(Blocks::Column::TxCount).eq(txCount));
+        tx_count_filter = tx_count_filter.add(
+            Expr::col(Blocks::Column::TxCount)
+                .eq(txCount)
+                .into_condition(),
+        );
+    }
+
+    //check for Page and size
+    let mut page_no: u8 = 0;
+    let mut page_size: u8 = 0;
+    if let Some(pn) = query_params.page {
+        page_no = pn;
+    }
+
+    if let Some(ps) = query_params.size {
+        page_size = ps;
+    }
+
+    // This line is used to get All Blocks.
+    //let all_blocks = BlockEntity::find().all(&db).await.unwrap();
+
+    let all_blocks = BlockEntity::find()
+        .filter(tx_count_filter)    // commit this with the URL http "localhost:3000/api/blocks?txCount=120" 
+        .all(&db)
+        .await
+        .unwrap();
+
+    // .order_by(Blocks::Column::Timestamp , Order::Asc)
+    // .paginate(&db, (page_no as u64) * ( page_size as u64))
 
     let blocks_response: Vec<BlockDataResponse> = all_blocks
         .iter()
