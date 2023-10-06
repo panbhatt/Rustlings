ef4ff9810e0a5ef8832b9b96dbee60b115a7d91d
“pankaj <panbhatt@gmail.com>
2023-10-06 13:41:16 -0400

    Finishing Logging

--- Useful_code/src/bin/log_env_logger.rs
+++ Useful_code/src/bin/log_env_logger.rs
@@ -1,27 +1,38 @@
-use env_logger::{ Builder, Target};
+use env_logger::{Builder, Target};
 use log::{debug, error, info, trace, warn};
 use std::env;
 
-fn main() {
-    let mut builder = Builder::from_default_env();
-    builder.target(Target::Stdout);
-    builder.
-    builder.parse_env(&env::var("MY_RUST_LOG").unwrap_or_default()).init(); 
-        
+// This one is not seems to be working: https://rust-lang-nursery.github.io/rust-cookbook/development_tools/debugging/config_log.html
 
-        println!("{}", env::var("MY_RUST_LOG").unwrap_or_default()); 
+fn main() {
+    /*let mut builder = Builder::new();
+    //builder.target(Target::Stdout);
+    builder.parse_env(&env::var("MY_RUST_LOG").unwrap_or_default()).init();
+
+
+        //println!("{}", env::var("MY_RUST_LOG").unwrap_or_default());
     // RUST_LOG CAN BE ANY EN VVARIABLE.
 
     info!("PANKAJ BHATT");
-    warn!("WARNING PANKAJ BHATT");
-    debug!("DEBUB PANKAJ BHATT"); 
+    log::warn!("WARNING PANKAJ BHATT");
+    debug!("DEBUB PANKAJ BHATT");
     error!("ERROR PANKAJ BHATT");
+    trace!("ERROR PANKAJ BHATT");
 
-    call_me();
-    
+    call_me();*/
+
+    Builder::new()
+        .parse_env(&env::var("MY_APP_LOG").unwrap_or_default())
+        .init();
+
+    log::info!("informational message");
+    log::warn!("warning message");
+    log::error!("this is an error {}", "message");
+
+
 }
 fn call_me() {
-    info!("Call me -> INFO"); 
+    info!("Call me -> INFO");
 }
 
-// https://www.youtube.com/watch?v=7EYDaSNSkbM 
+// https://www.youtube.com/watch?v=7EYDaSNSkbM
