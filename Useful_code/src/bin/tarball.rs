f42f655a6d0a82bb20d9253dce801eb8abbebac2
“pankaj <panbhatt@gmail.com>
2023-10-12 15:26:40 -0400

    ZIP-TARGZ -> Uncompressing the TAR file in a directory.

--- Useful_code/src/bin/tarball.rs
+++ Useful_code/src/bin/tarball.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
-use flate2::{write::GzEncoder, Compression};
-use tar; 
+use flate2::{write::GzEncoder, Compression, read::GzDecoder};
+use tar::{self, Archive}; 
 
 
 
@@ -8,6 +8,10 @@ fn main() {
     println!("COMPRESSING A FILE "); 
     create_tar("./src".to_owned(), "archive.tar.gz".to_owned());  // This will compress the SRC directory in the archive.tar.gz 
     println!("COMPRESSION DONE ");
+
+    println!("=============== STARTING DECOMPRESSION ==================="); 
+    decompress_tar("archive.tar.gz".to_owned(), "./srcclone".to_owned());
+    println!("============== UNCOMPRESSION DONE IN srclone dir = ================"); 
 
 }
 
@@ -18,7 +22,15 @@ fn create_tar(dir : String, archive_name : String) {
     let tar_gz = File::create(archive_name).unwrap();
     let encoder = GzEncoder::new(tar_gz, Compression::default()); 
     let mut tar = tar::Builder::new(encoder); 
-    tar.append_dir_all("*", dir); 
+    tar.append_dir_all("./", dir); 
 
+}
+
+fn decompress_tar(tar_file_name : String, untar_dir : String ) {
+
+    let tar_file = File::open(tar_file_name).unwrap();
+    let decoder = GzDecoder::new(tar_file);
+    let mut archive = Archive::new(decoder); 
+    archive.unpack(untar_dir);  
 
 }
