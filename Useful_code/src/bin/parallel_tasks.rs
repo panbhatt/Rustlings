233cc710c47efef350ab177765ca660c6f00610f
“pankaj <panbhatt@gmail.com>
2023-11-01 23:31:18 -0400

    RAYON -> All / Any parallelize methods

--- Useful_code/src/bin/parallel_tasks.rs
+++ Useful_code/src/bin/parallel_tasks.rs
@@ -1,7 +1,16 @@
 use rayon::prelude::*;
 
 fn main() {
+
+    // Mutate items in parallel
     let mut arr = [01, 2, 3, 4, 5, 6];
     arr.par_iter_mut().for_each(|p| *p += 1);
     println!("{:?}", arr);
+
+    // Test if any or all elements of a collection matchs a given predicate. 
+
+    let mut ages = vec![3,45,40,20,24,12, 8,9]; 
+
+    println!("All Even Ages -> {:?} ", ages.par_iter().all(|p| p > &0)); // All matches the condition. 
+    println!("Any Even Ages -> {:?} ", ages.par_iter().any(|p| p%2 == 0)); // Any matches the condition. 
 }
