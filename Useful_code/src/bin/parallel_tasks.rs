8a34ef9c7bf4c068af1b9f83ab4ce1022a61b54c
“pankaj <panbhatt@gmail.com>
2023-11-09 14:31:38 -0500

    RAYON -> Map Reduce with filter/map/sum/count

--- Useful_code/src/bin/parallel_tasks.rs
+++ Useful_code/src/bin/parallel_tasks.rs
@@ -1,6 +1,10 @@
 use rand::{thread_rng, distributions::Alphanumeric, Rng};
 use rayon::prelude::*;
 
+struct Person {
+    age : u32, 
+}
+
 fn main() {
 
     // Mutate items in parallel
@@ -28,5 +32,14 @@ fn main() {
 
     unsorted_vec.sort_unstable();
     println!("SORTED STRING -> {:?}", unsorted_vec); 
+
+    // MAP REDUCE in PARALLEL. 
+    let person_list = vec![Person{ age: 23},Person{ age: 02},Person{ age: 26},Person{ age: 28},Person{ age: 13},Person{ age: 32}]; 
+
+    let persons_over_30 = person_list.par_iter().filter(|&x| x.age > 30).count(); 
+    println!("Persons over 30 -> {}", persons_over_30);
+
+    let sum_over_25 : u32 = person_list.par_iter().map(|x| x.age).filter(|x| x > &25).sum() ; 
+    println!("Sum of Ages over 25 =  {} ", sum_over_25); 
 
 }
