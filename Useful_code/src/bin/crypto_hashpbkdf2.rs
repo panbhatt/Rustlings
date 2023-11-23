74d8cfe5871aecb8f6594227c47c6531bb5f4f8f
“pankaj <panbhatt@gmail.com>
2023-11-23 14:12:29 -0500

    CRYPTO -> Refactoring SALT n ENCRYPTED function

--- Useful_code/src/bin/crypto_hashpbkdf2.rs
+++ Useful_code/src/bin/crypto_hashpbkdf2.rs
@@ -12,27 +12,37 @@ fn main() -> Result<(),Unspecified> {
     let password = "ABCKDSDSDF&3434;";
     println!("Password = {}", password);
 
-    let encrypted_password = hash_password_with_salt(password);
-    println!("Entrypted Password = {}", encrypted_password);
+    let salt = getSalt(); 
+    let encrypted_password = encrypt_password(password, salt.clone());
+    println!("SALT = {} , Entrypted Password = {}",  salt, encrypted_password);
 
     Ok(())
 
 }
 
-fn hash_password_with_salt(pwd : &str)-> String {
-
+fn getSalt() -> String {
+    
     const CREDENTIAL_LEN : usize = digest::SHA512_256_OUTPUT_LEN; 
-    let n_iter = NonZeroU32::new(100_000).unwrap(); 
     let rng = rand::SystemRandom::new(); 
-
     let mut salt = [0u8; CREDENTIAL_LEN]; 
     rng.fill(&mut salt); 
 
-    println!("SALT = {}", HEXUPPER.encode(&salt)); 
+    return HEXUPPER.encode(&salt);
+}
+
+fn encrypt_password(pwd : &str, salt : String)-> String {
+
+    const CREDENTIAL_LEN : usize = digest::SHA512_256_OUTPUT_LEN; 
+    let n_iter = NonZeroU32::new(100_000).unwrap(); 
+
 
     let mut pbkdf2_hash = [ 0u8; CREDENTIAL_LEN]; 
-    pbkdf2::derive(pbkdf2::PBKDF2_HMAC_SHA512, n_iter, &salt, pwd.as_bytes(), &mut pbkdf2_hash);
+    pbkdf2::derive(pbkdf2::PBKDF2_HMAC_SHA512, n_iter, salt.as_bytes(), pwd.as_bytes(), &mut pbkdf2_hash);
 
 
     return HEXUPPER.encode(&pbkdf2_hash);
+}
+
+fn decrypt_password() -> String {
+    return "".to_string()
 }
